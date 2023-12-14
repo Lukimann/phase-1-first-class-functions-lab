d6f08f1ac94cd1bca69eaf6b3efda242787cdacd
Luka Wanyama <lukewanyama5@gmail.com>
2023-12-14 07:28:00 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+
+}
+const firstTwoDrivers = returnFirstTwoDrivers(drivers);
+console.log(firstTwoDrivers);
+
+
+
+const driver = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnLastTwoDrivers = function(driver) {
+    return driver.slice(-2)
+
+}
+const lastTwoDrivers = returnLastTwoDrivers(driver);
+console.log(lastTwoDrivers)
+
+const createFareMultiplier = function(multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    }
+     
+}
+
+const fareDoubler = function(doubler = * 2) {
+    return function (fare) {
+        return fare * doubler;
+    }
+}
